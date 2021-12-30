609d177650cc980549973ac06211fbe0df9b7e3f
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2021-12-30 20:43:37 +0100

    Add models

---
+++ RecipeManagerWeb/Models/GroceryCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeManagerWeb.Models
+{
+    public class GroceryCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RecipeManagerWeb/Models/InstructionStep.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeManagerWeb.Models
+{
+    public class InstructionStep
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
