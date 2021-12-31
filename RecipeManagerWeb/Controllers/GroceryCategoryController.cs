2e7ccdd6cbb51085be72d2136a961700405bde80
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2021-12-31 18:21:36 +0100

    Add swagger and GroceryCategory post

---
+++ RecipeManagerWeb/Controllers/GroceryCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeManagerWeb.Repositories;
+
+namespace RecipeManagerWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroceryCategoryController : ControllerBase
+    {
+        private readonly IGroceryCategoryRepository _groceryCategoryRepositoy;
+
+        public GroceryCategoryController(IGroceryCategoryRepository groceryCategoryRepository) 
+        { 
+            _groceryCategoryRepositoy = groceryCategoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddGroceryCategory(string name)
+        {
+            return Ok(await _groceryCategoryRepositoy.AddGroceryCategory(name));
+        }
+    }
+}
---
+++ RecipeManagerWeb/Repositories/IGroceryCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using RecipeManagerWeb.Models;
+
+namespace RecipeManagerWeb.Repositories
+{
+    public interface IGroceryCategoryRepository
+    {
+        public Task<GroceryCategory> AddGroceryCategory(string categoryName);
+    }
+}
