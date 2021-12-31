8f669800abdb86c902c997cb149e297f1861d078
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2021-12-31 12:24:44 +0100

    Update db connection

--- RecipeManagerWeb/Data/DataContext.cs
+++ RecipeManagerWeb/Data/DataContext.cs
@@ -5,17 +5,14 @@ namespace RecipeManagerWeb.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
         public DbSet<GroceryItem> GroceryItems { get; set; }
         public DbSet<GroceryCategory> GroceryCategories { get; set; }
         public DbSet<InstructionStep> InstructionSteps { get; set; } 
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<RecipeCategory> RecipeCategories { get; set; }
         public DbSet<RecipeGroceryItem> RecipeGroceryItems { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=recipemanager.db;");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
