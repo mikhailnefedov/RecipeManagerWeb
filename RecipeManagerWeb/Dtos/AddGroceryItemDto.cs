b1d8abdd6d97bb028ac0ee1464ad58cd3c991f79
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2022-02-21 22:15:05 +0100

    Fix nullability

--- RecipeManagerWeb/Dtos/AddGroceryItemDto.cs
+++ RecipeManagerWeb/Dtos/AddGroceryItemDto.cs
@@ -2,7 +2,7 @@
 {
     public class AddGroceryItemDto
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = String.Empty;
         public int GroceryCategoryId { get; set; } 
     }
 }
