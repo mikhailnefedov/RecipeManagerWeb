a0c58271e0de101187f29380a0f33a1f2bba008d
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2022-02-23 16:46:35 +0100

    Add test project
    Implement add grocery category test

---
+++ RecipeManagerWebTest/Repositories/GroceryCategoryRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using RecipeManagerWeb.Data;
+using RecipeManagerWeb.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeManagerWebTest.Repositories
+{
+    public class GroceryCategoryRepositoryTest
+    {
+        private readonly GroceryCategoryRepository _groceryCategoryRepository;
+
+        public GroceryCategoryRepositoryTest()
+        {
+            var dbOptions = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "testDB")
+                .Options;
+
+            var context = new DataContext(dbOptions);
+
+            _groceryCategoryRepository = new GroceryCategoryRepository(context);
+        }
+
+        [Test]
+        public void AddGroceryCategory_MinimalGroceryCategory_NoErrors()
+        {
+            string categoryName = "category";
+
+            Assert.DoesNotThrowAsync(async () => 
+            { 
+                await _groceryCategoryRepository.AddGroceryCategory(categoryName);
+            });
+                       
+        }
+    }
+}
