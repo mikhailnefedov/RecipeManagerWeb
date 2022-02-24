4e139d970d59fb98deff02342074b01c7508ac6c
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2022-02-24 10:53:42 +0100

    Implement post recipe and recipe category tests

--- RecipeManagerWeb/Dtos/AddRecipeGroceryItemDto.cs
+++ RecipeManagerWeb/Dtos/AddRecipeGroceryItemDto.cs
@@ -4,7 +4,6 @@ namespace RecipeManagerWeb.Dtos
 {
     public class AddRecipeGroceryItemDto
     {
-        public int RecipeId { get; set; }
         public int GroceryItemId { get; set; }
         public double Amount { get; set; }
         public MeasurementUnit Measurementunit { get; set; }
