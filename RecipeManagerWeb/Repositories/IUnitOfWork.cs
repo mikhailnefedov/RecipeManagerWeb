9f42fdf9bee7953544df03f0e81bd73fc0f51c3f
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2022-08-04 12:41:20 +0200

    Add UnitOfWork

---
+++ RecipeManagerWeb/Repositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipeManagerWeb.Repositories
+{
+    public interface IUnitOfWork
+    {
+        GroceryItemRepository GroceryItemRepository { get; }
+        GroceryCategoryRepository GroceryCategoryRepository { get; }
+        InstructionStepRepository InstructionStepRepository { get; }
+        RecipeCategoryRepository RecipeCategoryRepository { get; }
+        RecipeRepository RecipeRepository { get; }
+
+        Task<int> Save();
+    }
+}
---
+++ RecipeManagerWeb/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using RecipeManagerWeb.Data;
+
+namespace RecipeManagerWeb.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+
+        public GroceryItemRepository GroceryItemRepository { get; }
+
+        public GroceryCategoryRepository GroceryCategoryRepository { get; }
+
+        public InstructionStepRepository InstructionStepRepository { get; }
+
+        public RecipeCategoryRepository RecipeCategoryRepository { get; }
+
+        public RecipeRepository RecipeRepository { get; }
+
+        public UnitOfWork(DataContext context,
+            GroceryItemRepository groceryItemRepository,
+            GroceryCategoryRepository groceryCategoryRepository,
+            InstructionStepRepository instructionStepRepository,
+            RecipeCategoryRepository recipeCategoryRepository,
+            RecipeRepository recipeRepository)
+        {
+            _context = context;
+            GroceryItemRepository = groceryItemRepository;
+            GroceryCategoryRepository = groceryCategoryRepository;
+            InstructionStepRepository = instructionStepRepository;
+            RecipeCategoryRepository = recipeCategoryRepository;
+            RecipeRepository = recipeRepository;
+        }
+
+        public async Task<int> Save()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
