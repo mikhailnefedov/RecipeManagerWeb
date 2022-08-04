10549b880505d67d3d497303cf0f6dc6dd7083e1
mikhailnefedov <52661281+mikhailnefedov@users.noreply.github.com>
2022-08-04 15:28:36 +0200

    Update repository to unitofwork usage

--- RecipeManagerWeb/Repositories/IGroceryCategoryRepository.cs
+++ RecipeManagerWeb/Repositories/IGroceryCategoryRepository.cs
@@ -5,7 +5,7 @@ namespace RecipeManagerWeb.Repositories
 {
     public interface IGroceryCategoryRepository
     {
-        public Task<GroceryCategory> AddGroceryCategory(string categoryName);
+        public Task<GetGroceryCategoryDto> AddGroceryCategory(string categoryName);
         public Task<GetGroceryCategoryDto?> GetGroceryCategory(int groceryCategoryId);
         public Task<List<GetGroceryCategoryDto>> GetGroceryCategories();
         public Task<bool> DeleteGroceryCategory(int groceryCategoryId);
